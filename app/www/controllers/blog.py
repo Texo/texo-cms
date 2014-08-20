4be0fd12b4d19a62008f34af6b894b82a531e2d0
Adam Presley <adam@adampresley.com>
2014-08-20 00:56:45 -0500

    Doing the markdown rendering here

--- app/www/controllers/blog.py
+++ app/www/controllers/blog.py
@@ -1,19 +1,13 @@
 import config
+import markdown
 import requests
 
 from bottle import route
 from bottle import template
-from bottle import request
-from bottle import response
-from bottle import static_file
 from bottle import mako_view as view
 from bottle import mako_template as template
 
-from datetime import datetime
-
-from services.http import httpservice
 from services.publicapi import blogapi
-from services.engine import postservice
 from services.engine import engineservice
 
 ###############################################################################
@@ -24,26 +18,34 @@ from services.engine import engineservice
 @route("/", method="GET")
 @view("posts.html")
 def index():
-	posts = requests.get(engineservice.buildUrl("/posts/1"))
-	return blogapi.decorateDictionaryWithAPI(object=posts.json())
+	posts = requests.get(engineservice.buildUrl("/posts/1")).json()
+	posts["posts"] = map(renderMarkdown, posts["posts"])
+
+	return blogapi.decorateDictionaryWithAPI(object=posts)
 
 @route("/posts/<page:int>", method="GET")
 @view("posts.html")
 def getPosts(page):
-	posts = requests.get(engineservice.buildUrl("/posts/%d" % (page,)))
-	return blogapi.decorateDictionaryWithAPI(object=posts.json())
+	posts = requests.get(engineservice.buildUrl("/posts/%d" % (page,))).json()
+	posts["posts"] = map(renderMarkdown, posts["posts"])
+
+	return blogapi.decorateDictionaryWithAPI(object=posts)
 
 @route("/posts/search/<searchTerm>.json", method="GET")
 def getPostsBySearchAsJson(searchTerm):
-	posts = requests.get(engineservice.buildUrl("/posts/1/search/%s" % (searchTerm,)))
-	return posts.json()
+	posts = requests.get(engineservice.buildUrl("/posts/1/search/%s" % (searchTerm,))).json()
+	posts["posts"] = map(renderMarkdown, posts["posts"])
+
+	return posts
 
 @route("/posts/<tag>", method="GET")
 @route("/posts/<tag>/<page:int>", method="GET")
 @view("posts.html")
 def getPostsByTag(tag, page=1):
-	posts = requests.get(engineservice.buildUrl("/posts/%d/tag/%s" % (page, tag,)))
-	result = blogapi.decorateDictionaryWithAPI(object=posts.json())
+	posts = requests.get(engineservice.buildUrl("/posts/%d/tag/%s" % (page, tag,))).json()
+	posts["posts"] = map(renderMarkdown, posts["posts"])
+
+	result = blogapi.decorateDictionaryWithAPI(object=posts)
 	result["tag"] = tag
 	return result
 
@@ -54,5 +56,23 @@ def getPostsByTag(tag, page=1):
 @route("/post/<year:int>/<month:int>/<slug>", method="GET")
 @view("post.html")
 def getPostByDateAndSlug(year, month, slug):
-	post = requests.get(engineservice.buildUrl("/post/%d/%d/%s" % (year, month, slug,)))
-	return blogapi.decorateDictionaryWithAPI(object=post.json())
+	post = requests.get(engineservice.buildUrl("/post/%d/%d/%s" % (year, month, slug,))).json()
+	post["post"] = renderMarkdown(post["post"])
+
+	return blogapi.decorateDictionaryWithAPI(object=post)
+
+
+def renderMarkdown(post):
+	extensions = [
+		"codehilite(css_class=highlight",
+		"tables",
+		"extra",
+	]
+
+	if config.CODE_LINE_NUMBERS:
+		extensions[0] = extensions[0] + ",linenums=True)"
+	else:
+		extensions[0] = extensions[0] + ")"
+
+	post["content"] = markdown.markdown(post["content"].decode("utf-8"), output_format="html5", extensions=extensions)
+	return post
