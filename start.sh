f58857e1959e85b072b1627c909cd274ac16b06f
Adam Presley <adam@adampresley.com>
2014-08-19 23:22:58 -0500

    Added required environment variables for the services server address

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ export PYTHONPATH="./:./app"
 export PYTHONDONTWRITEBYTECODE="True"
 export TEXO_SETTINGS_DEBUG="True"
 
+export TEXO_SERVER_ADDRESS=localhost
+export TEXO_SERVER_PORT=8081
+
 source ./virtualenv/bin/activate
 python -B ./app/www/texo.py
 deactivate
