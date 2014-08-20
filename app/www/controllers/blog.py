0a4b266eab193a3c961e85fed78b985a840ff307
Adam Presley <adam@adampresley.com>
2014-08-19 23:36:35 -0500

    A single post is now retrieved from the service application

--- app/www/controllers/blog.py
+++ app/www/controllers/blog.py
@@ -54,7 +54,8 @@ def getPostsByTag(tag, page=1):
 @route("/post/<year:int>/<month:int>/<slug>", method="GET")
 @view("post.html")
 def getPostByDateAndSlug(year, month, slug):
-	return blogapi.decorateDictionaryWithAPI(object=_getPost(year=year, month=month, slug=slug))
+	post = requests.get(engineservice.buildUrl("/post/%d/%d/%s" % (year, month, slug,)))
+	return blogapi.decorateDictionaryWithAPI(object=post.json())
 
 
 ###############################################################################
@@ -74,53 +75,3 @@ def getTagByIdAsJson(id):
 		return postservice.makeFriendlyTag(tag=tag)
 	else:
 		return httpservice.notFound(response=response, message="Tag not found")
-
-
-###############################################################################
-# Private methods
-###############################################################################
-
-def _getPosts(page, tag=None, dateRange=None):
-	posts, postCount, numPages = postservice.getPosts(page=page, status="Published", postsPerPage=config.POSTS_PER_PAGE, tag=tag)
-
-	return {
-		"posts": map(postservice.makePageFriendlyPost, posts),
-		"numPages": int(numPages),
-		"numPosts": int(postCount),
-		"currentPage": 0 if postCount <= 0 else int(page),
-		"previousPage": 1 if page < 2 else int(page - 1),
-		"nextPage": page if page >= numPages else int(page + 1),
-		"lastPage": int(numPages),
-		"showFirstPageNavButton": True if postCount > 0 and page > 1 else False,
-		"showLastPageNavButton": True if postCount > 0 and page < numPages else False,
-		"showNextPageNavButton": True if postCount > 0 and page < numPages else False,
-		"showPrevPageNavButton": True if postCount > 0 and page > 1 else False,
-		"showPageNavigation": True if postCount > 0 and numPages > 1 else False,
-	}
-
-def _getPost(year, month, slug):
-	post = postservice.getPostByDateAndSlug(year=year, month=month, slug=slug)
-
-	return {
-		"post": postservice.makePageFriendlyPost(post),
-		"numPages": 1,
-		"numPosts": 1,
-		"currentPage": 1,
-		"previousPage": 1,
-		"nextPage": 1,
-		"lastPage": 1,
-		"showFirstPageNavButton": False,
-		"showLastPageNavButton": False,
-		"showNextPageNavButton": False,
-		"showPrevPageNavButton": False,
-		"showPageNavigation": False,
-	}
-
-def _searchPosts(searchTerm):
-	posts, postCount = postservice.searchPosts(searchTerm=searchTerm)
-
-	return {
-		"posts": map(postservice.makePageFriendlyPost, posts),
-		"numPosts": int(postCount),
-	}
-
